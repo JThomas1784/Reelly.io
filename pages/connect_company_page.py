ea864adeea216b5eee83c391dc736e08854a3260
JThomas1784 <151489645+JThomas1784@users.noreply.github.com>
2024-11-21 03:42:31 -0500

    Update connect_company_page.py

--- pages/connect_company_page.py
+++ pages/connect_company_page.py
@@ -4,5 +4,5 @@ class ConnectCompanyPage:
 
     def is_correct_page_open(self):
         """Verifies that the correct page opened by checking the URL."""
-        expected_url = "https://soft.reelly.io/expected_page_after_connect"  # Replace with actual URL
+        expected_url = "https://soft.reelly.io/payment/personal"
         return self.driver.current_url == expected_url
